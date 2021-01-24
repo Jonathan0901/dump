5d5132fc43d465b007904b5ef6f61b8afca2eecf
Jonathan0901 <jonathandelacruzalvarez@gmail.com>
2021-01-23 20:43:52 -0600

    subir

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace joan\dump;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
